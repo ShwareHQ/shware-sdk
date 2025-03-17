4ffc3be1ee75b63ce27eee46aef654159c8fcc3e
hdsuperman <zhd.superman@gmail.com>
2025-03-17 14:08:07 +0800

    chore: rename workerId to machineId

--- packages/http/src/snowflake.ts
+++ packages/http/src/snowflake.ts
@@ -20,19 +20,19 @@ function getNextMillisecond(timestamp: bigint) {
 
 export class UidGenerator {
   public readonly MAX = (1n << 63n) - 1n;
-  private readonly workerBits = BigInt(10);
+  private readonly machineBits = BigInt(10);
   private readonly sequenceBits = BigInt(12);
   private readonly epochMillisecond = BigInt(1577808000000); // 2020-1-1 00:00:00
   private readonly sequenceMask = ~(BigInt(-1) << this.sequenceBits);
-  private readonly maxWorkerId = ~(BigInt(-1) << this.workerBits);
-  private readonly workerIdShift = this.sequenceBits;
-  private readonly timeStampShift = this.workerBits + this.sequenceBits;
-  private readonly workerId: bigint;
+  private readonly maxMachineId = ~(BigInt(-1) << this.machineBits);
+  private readonly machineIdShift = this.sequenceBits;
+  private readonly timeStampShift = this.machineBits + this.sequenceBits;
+  private readonly machineId: bigint;
   private sequence = BigInt(0);
   private lastTimestamp = BigInt(-1);
 
-  constructor(workerId: number) {
-    this.workerId = BigInt(workerId) % this.maxWorkerId;
+  constructor(mackineId: number) {
+    this.machineId = BigInt(mackineId) % this.maxMachineId;
   }
 
   public readonly next = (): bigint => {
@@ -52,11 +52,11 @@ export class UidGenerator {
     this.lastTimestamp = timestamp;
     return (
       ((timestamp - this.epochMillisecond) << this.timeStampShift) |
-      (this.workerId << this.workerIdShift) |
+      (this.machineId << this.machineIdShift) |
       this.sequence
     );
   };
 }
 
-const workerId = Math.floor(Math.random() * 1024);
-export const uid = new UidGenerator(workerId);
+const machineId = Math.floor(Math.random() * 1024);
+export const uid = new UidGenerator(machineId);
