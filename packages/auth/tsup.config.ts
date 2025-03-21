df11e96c055acfa9b3751b9bcc3891f2d70a04fc
hdsuperman <zhd.superman@gmail.com>
2025-03-21 23:30:02 +0800

    feat: null type

---
+++ packages/auth/tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, Options } from 'tsup';
+
+const options: Options = {
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: false,
+  dts: true,
+  format: ['esm', 'cjs'],
+  outExtension: ({ format }) => (format === 'esm' ? { js: '.mjs' } : { js: '.cjs' }),
+};
+
+export default defineConfig([
+  {
+    ...options,
+    entry: ['src/**/*.ts', 'src/**/*.tsx'],
+    outDir: 'dist',
+    esbuildPlugins: [
+      {
+        name: 'add-extension',
+        setup(build) {
+          const defaultExtension = build.initialOptions.format === 'esm' ? '.mjs' : '.cjs';
+          const extension = build.initialOptions.outExtension?.js ?? defaultExtension;
+          build.onResolve({ filter: /.*/ }, (args) => {
+            if (args.importer) return { path: args.path + extension, external: true };
+          });
+        },
+      },
+    ],
+  },
+]);
--- packages/http/src/hono/geolocation.ts
+++ packages/http/src/hono/geolocation.ts
@@ -1,50 +1,53 @@
 import type { Context } from 'hono';
 
 export type Geolocation = {
-  ip?: string;
-  city?: string;
-  country?: string;
-  continent?: string;
-  longitude?: number;
-  latitude?: number;
-  region?: string;
-  region_code?: string;
-  metro_code?: string;
-  postal_code?: string;
-  timezone?: string;
+  ip: string | null;
+  city: string | null;
+  country: string | null;
+  continent: string | null;
+  longitude: number | null;
+  latitude: number | null;
+  region: string | null;
+  region_code: string | null;
+  metro_code: string | null;
+  postal_code: string | null;
+  timezone: string | null;
 };
 
 /** reference: https://developers.cloudflare.com/rules/transform/managed-transforms/reference/#add-visitor-location-headers */
 export function geolocation(c: Context): Geolocation {
   return {
-    ip: c.req.header('true-client-ip') ?? c.req.header('cf-connecting-ip'),
-    city: c.req.header('cf-ipcity'),
-    country: c.req.header('cf-ipcountry'),
-    continent: c.req.header('cf-ipcontinent'),
-    longitude: c.req.header('cf-iplongitude') ? Number(c.req.header('cf-iplongitude')) : undefined,
-    latitude: c.req.header('cf-iplatitude') ? Number(c.req.header('cf-iplatitude')) : undefined,
-    region: c.req.header('cf-region'),
-    region_code: c.req.header('cf-region-code'),
-    metro_code: c.req.header('cf-metro-code'),
-    postal_code: c.req.header('cf-postal-code'),
-    timezone: c.req.header('cf-timezone'),
+    ip: c.req.header('true-client-ip') ?? c.req.header('cf-connecting-ip') ?? null,
+    city: c.req.header('cf-ipcity') ?? null,
+    country: c.req.header('cf-ipcountry') ?? null,
+    continent: c.req.header('cf-ipcontinent') ?? null,
+    longitude: c.req.header('cf-iplongitude') ? Number(c.req.header('cf-iplongitude')) : null,
+    latitude: c.req.header('cf-iplatitude') ? Number(c.req.header('cf-iplatitude')) : null,
+    region: c.req.header('cf-region') ?? null,
+    region_code: c.req.header('cf-region-code') ?? null,
+    metro_code: c.req.header('cf-metro-code') ?? null,
+    postal_code: c.req.header('cf-postal-code') ?? null,
+    timezone: c.req.header('cf-timezone') ?? null,
   };
 }
 
 /** https://github.com/vercel/vercel/blob/main/packages/functions/src/headers.ts */
 export function geolocationVercel(c: Context): Geolocation {
   return {
-    ip: c.req.header('x-real-ip'),
-    city: c.req.header('x-vercel-ip-city'),
-    country: c.req.header('x-vercel-ip-country'),
-    continent: c.req.header('x-vercel-ip-continent'),
+    ip: c.req.header('x-real-ip') ?? null,
+    city: c.req.header('x-vercel-ip-city') ?? null,
+    country: c.req.header('x-vercel-ip-country') ?? null,
+    continent: c.req.header('x-vercel-ip-continent') ?? null,
     longitude: c.req.header('x-vercel-ip-longitude')
       ? Number(c.req.header('x-vercel-ip-longitude'))
-      : undefined,
+      : null,
     latitude: c.req.header('x-vercel-ip-latitude')
       ? Number(c.req.header('x-vercel-ip-latitude'))
-      : undefined,
-    region_code: c.req.header('x-vercel-ip-country-region'),
-    postal_code: c.req.header('x-vercel-ip-postal-code'),
+      : null,
+    region: null,
+    region_code: c.req.header('x-vercel-ip-country-region') ?? null,
+    metro_code: null,
+    postal_code: c.req.header('x-vercel-ip-postal-code') ?? null,
+    timezone: null,
   };
 }
