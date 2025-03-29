9993365fc31359ba8e860bad2dd0076d97a7b088
hdsuperman <zhd.superman@gmail.com>
2025-03-29 23:27:30 +0800

    feat: export types

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,5 @@
 export { OAuth2 } from './oauth2';
 export { oauth2RedirectQuerySchema } from './schema';
 export { RedisIndexedSessionRepository, PRINCIPAL_NAME_INDEX_NAME } from './session/redis-session';
-export type { Namespace, SessionRepository } from './session/types';
+export type { Namespace, SessionRepository, Principal } from './session/types';
+export type { StandardClaims, OAuth2Token } from './client';
