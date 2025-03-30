bac9a427d34ea99af5919d6295c3667efd902851
hdsuperman <zhd.superman@gmail.com>
2025-03-30 15:24:03 +0800

    feat: add route

--- packages/auth/src/hono/index.ts
+++ packages/auth/src/hono/index.ts
@@ -7,6 +7,7 @@ import type { OAuth2Config, OAuth2AuthorizationRequest } from '../oauth2';
 import { CookieOptions } from 'hono/utils/cookie';
 import type { Principal } from '../session/types';
 import { OAuth2Token, StandardClaims } from '../client';
+import { oauth2RedirectQuerySchema } from '../schema';
 
 type Cookie = Omit<CookieOptions, 'expires' | 'maxAge'>;
 
@@ -15,12 +16,12 @@ export interface Config {
   repository: SessionRepository;
   oauth2: OAuth2Config;
   onLoggedChecked?: (principal: Principal) => void | Promise<void>;
-  onAuthorized?: (
+  onAuthorized: (
     c: Context,
     registrationId: string,
     claims: StandardClaims,
     token: OAuth2Token
-  ) => void;
+  ) => Principal | Promise<Principal>;
 }
 
 const SESSION_COOKIE_NAME = 'SESSION';
@@ -35,6 +36,10 @@ const defaultCookieOptions: Cookie = {
 export function createOAuth2App({ repository, ...config }: Config) {
   const app = new Hono();
   const oauth2 = new OAuth2(config.oauth2);
+
+  function redirect(c: Context, error: string) {
+    return c.redirect(`${config.oauth2.client.errorUri}?error=${error}`, 302);
+  }
 
   app.get('/logged', async (c) => {
     const sessionId = getCookie(c, SESSION_COOKIE_NAME);
@@ -87,5 +92,56 @@ export function createOAuth2App({ repository, ...config }: Config) {
     });
 
     return c.redirect(uri.href, 302);
+  });
+
+  app.get('/login/oauth2/code/:registrationId', async (c) => {
+    // 1. get session from cookie
+    const sessionId = getCookie(c, SESSION_COOKIE_NAME);
+    const session = sessionId ? await repository.findById(sessionId) : null;
+    if (!session) {
+      return redirect(c, 'OAUTH2_AUTHORIZATION_SESSION_NOT_FOUND');
+    }
+
+    // 2. get authorization request from session
+    const json = session.getAttribute(OAUTH2_AUTHORIZATION_REQUEST_ATTR) as string | null;
+    if (!json) {
+      return redirect(c, 'OAUTH2_AUTHORIZATION_REQUEST_NOT_FOUND');
+    }
+
+    // 3. validate redirect query
+    const cached: OAuth2AuthorizationRequest = JSON.parse(json);
+    const registrationId = c.req.param('registrationId');
+
+    const parsed = oauth2RedirectQuerySchema.safeParse(c.req.query());
+    if (
+      !parsed.success ||
+      parsed.data.state !== cached.state ||
+      registrationId !== cached.registrationId
+    ) {
+      return redirect(c, 'INVALID_OAUTH2_REDIRECT_QUERY');
+    }
+
+    // 4. exchange authorization code for token
+    const { code } = parsed.data;
+    const { codeVerifier } = cached;
+    const token = await oauth2.exchangeAuthorizationCode(registrationId, code, codeVerifier);
+    const { claims } = await oauth2.getUserInfo(registrationId, token);
+
+    // 5. create or update principal
+    const principal = await config.onAuthorized(c, registrationId, claims, token);
+
+    // 6. update session
+    session.setLastAccessedTime(Date.now());
+    session.removeAttribute(OAUTH2_AUTHORIZATION_REQUEST_ATTR);
+    session.setAttribute(PRINCIPAL_NAME_INDEX_NAME, principal.name);
+    await repository.save(session);
+
+    setCookie(c, SESSION_COOKIE_NAME, session.getId(), {
+      ...defaultCookieOptions,
+      ...config.cookie,
+      maxAge: session.getMaxInactiveInterval(),
+    });
+
+    return c.redirect(config.oauth2.client.baseUri, 302);
   });
 }
