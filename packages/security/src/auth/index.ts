fc9aff7fd5797c447ae171bce601cac281bf66f2
hdsuperman <zhd.superman@gmail.com>
2025-04-02 12:31:12 +0800

    chore: rename

--- packages/security/src/auth/index.ts
+++ packages/security/src/auth/index.ts
@@ -307,7 +307,7 @@ export class Auth implements AuthService {
     }
   };
 
-  authorize = async (request: Request): Promise<boolean> => {
+  isAuthorized = async (request: Request): Promise<boolean> => {
     const sessionId = getCookie(request, this.cookieName);
     if (!sessionId) return false;
     const session = await this.repository.findById(sessionId);
