036721ef9b8e6114d0c7c8c92c587da3857c8021
hdsuperman <zhd.superman@gmail.com>
2025-04-19 10:58:16 +0800

    fix: gtag items array

--- packages/analytics/src/schema/index.ts
+++ packages/analytics/src/schema/index.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 
+const items = z.array(
+  z.record(
+    z.string().trim().min(1).max(128),
+    z.union([z.string().max(512), z.number(), z.boolean(), z.null()])
+  )
+);
+
 export const createTrackEventSchema = z
   .object({
     name: z.string().trim().min(1).max(64),
@@ -44,13 +51,7 @@ export const createTrackEventSchema = z
     properties: z
       .record(
         z.string().trim().min(1).max(128),
-        z.union([
-          z.string().max(512),
-          z.number(),
-          z.boolean(),
-          z.null(),
-          z.array(z.union([z.string().max(512), z.number(), z.boolean(), z.null()])),
-        ])
+        z.union([z.string().max(512), z.number(), z.boolean(), z.null(), items])
       )
       .refine((data) => Object.keys(data).length <= 64)
       .optional(),
@@ -64,13 +65,7 @@ export const createVisitorSchema = z.object({
   properties: z
     .record(
       z.string().trim().min(1).max(128),
-      z.union([
-        z.string().max(512),
-        z.number(),
-        z.boolean(),
-        z.null(),
-        z.array(z.union([z.string().max(512), z.number(), z.boolean(), z.null()])),
-      ])
+      z.union([z.string().max(512), z.number(), z.boolean(), z.null()])
     )
     .refine((data) => Object.keys(data).length <= 64)
     .optional(),
@@ -80,13 +75,7 @@ export const updateVisitorSchema = z.object({
   properties: z
     .record(
       z.string().trim().min(1).max(128),
-      z.union([
-        z.string().max(512),
-        z.number(),
-        z.boolean(),
-        z.null(),
-        z.array(z.union([z.string().max(512), z.number(), z.boolean(), z.null()])),
-      ])
+      z.union([z.string().max(512), z.number(), z.boolean(), z.null()])
     )
     .refine((data) => Object.keys(data).length <= 64),
 });
