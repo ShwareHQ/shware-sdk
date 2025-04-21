f00ec0570cbe47e90dfedf56eb11aa6aac40521a
hdsuperman <zhd.superman@gmail.com>
2025-04-21 23:51:01 +0800

    feat: rename

--- packages/http/src/hono/geolocation.ts
+++ packages/http/src/hono/geolocation.ts
@@ -2,7 +2,7 @@ import { getRuntimeKey } from 'hono/adapter';
 import type { Context } from 'hono';
 
 export type Geolocation = {
-  ip: string | null;
+  ip_address: string | null;
   city: string | null;
   country: string | null; // ISO 3166-1 alpha-2
   continent: string | null;
@@ -11,7 +11,7 @@ export type Geolocation = {
   region: string | null; //  ISO 3166-2
   metro_code: string | null;
   postal_code: string | null;
-  timezone: string | null;
+  time_zone: string | null;
 };
 
 const runtime = getRuntimeKey();
@@ -19,7 +19,7 @@ export function geolocation(c: Context): Geolocation | null {
   /** reference: https://developers.cloudflare.com/rules/transform/managed-transforms/reference/#add-visitor-location-headers */
   if (runtime === 'workerd') {
     return {
-      ip: c.req.header('true-client-ip') ?? c.req.header('cf-connecting-ip') ?? null,
+      ip_address: c.req.header('true-client-ip') ?? c.req.header('cf-connecting-ip') ?? null,
       city: c.req.header('cf-ipcity') ?? null,
       country: c.req.header('cf-ipcountry') ?? null,
       continent: c.req.header('cf-ipcontinent') ?? null,
@@ -28,14 +28,14 @@ export function geolocation(c: Context): Geolocation | null {
       region: c.req.header('cf-region-code') ?? null,
       metro_code: c.req.header('cf-metro-code') ?? null,
       postal_code: c.req.header('cf-postal-code') ?? null,
-      timezone: c.req.header('cf-timezone') ?? null,
+      time_zone: c.req.header('cf-timezone') ?? null,
     };
   }
 
   /** https://github.com/vercel/vercel/blob/main/packages/functions/src/headers.ts */
   if (c.req.header('x-vercel-id')) {
     return {
-      ip: c.req.header('x-real-ip') ?? null,
+      ip_address: c.req.header('x-real-ip') ?? null,
       city: c.req.header('x-vercel-ip-city') ?? null,
       country: c.req.header('x-vercel-ip-country') ?? null,
       continent: c.req.header('x-vercel-ip-continent') ?? null,
@@ -48,14 +48,14 @@ export function geolocation(c: Context): Geolocation | null {
       region: c.req.header('x-vercel-ip-country-region') ?? null,
       metro_code: null,
       postal_code: c.req.header('x-vercel-ip-postal-code') ?? null,
-      timezone: null,
+      time_zone: null,
     };
   }
 
   // cloudfront
   // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/adding-cloudfront-headers.html#cloudfront-headers-viewer-location
   return {
-    ip: c.req.header('CloudFront-Viewer-Address') ?? null,
+    ip_address: c.req.header('CloudFront-Viewer-Address')?.split(':').at(0) ?? null,
     city: c.req.header('CloudFront-Viewer-City') ?? null,
     country: c.req.header('CloudFront-Viewer-Country') ?? null,
     continent: null,
@@ -68,6 +68,6 @@ export function geolocation(c: Context): Geolocation | null {
     region: c.req.header('CloudFront-Viewer-Country-Region') ?? null,
     metro_code: c.req.header('CloudFront-Viewer-Metro-Code') ?? null,
     postal_code: c.req.header('CloudFront-Viewer-Postal-Code') ?? null,
-    timezone: c.req.header('CloudFront-Viewer-Time-Zone') ?? null,
+    time_zone: c.req.header('CloudFront-Viewer-Time-Zone') ?? null,
   };
 }
