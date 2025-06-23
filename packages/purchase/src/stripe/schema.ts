9b97f73c3a13598d2dbf1c3437e5c03fdd82586f
hdsuperman <zhd.superman@gmail.com>
2025-06-23 21:39:53 +0800

    chore: nullable

--- packages/purchase/src/stripe/schema.ts
+++ packages/purchase/src/stripe/schema.ts
@@ -1,19 +1,30 @@
-import { maxLength, object, optional, string, enum as _enum, url, type z } from 'zod/v4-mini';
+import {
+  maxLength,
+  object,
+  optional,
+  nullable,
+  string,
+  enum as _enum,
+  url,
+  type z,
+} from 'zod/v4-mini';
 import type { ProductId } from './types';
 
 export const cancellationDetailsSchema = object({
-  comment: optional(string().check(maxLength(1024))),
+  comment: optional(nullable(string().check(maxLength(1024)))),
   feedback: optional(
-    _enum([
-      'customer_service',
-      'low_quality',
-      'missing_features',
-      'switched_service',
-      'too_complex',
-      'too_expensive',
-      'unused',
-      'other',
-    ])
+    nullable(
+      _enum([
+        'customer_service',
+        'low_quality',
+        'missing_features',
+        'switched_service',
+        'too_complex',
+        'too_expensive',
+        'unused',
+        'other',
+      ])
+    )
   ),
 });
 
@@ -26,5 +37,5 @@ export function checkoutSessionSchema(productIds: [ProductId, ...ProductId[]]) {
 }
 
 export interface CancellationDetails extends z.output<typeof cancellationDetailsSchema> {
-  reason?: 'cancellation_requested' | 'payment_disputed' | 'payment_failed';
+  reason?: 'cancellation_requested' | 'payment_disputed' | 'payment_failed' | null;
 }
