ff44a1c6ba2741d9e958aeda9ab0b74046a82098
hdsuperman <zhd.superman@gmail.com>
2025-06-23 22:16:41 +0800

    feat: add provider

--- packages/security/src/core/index.ts
+++ packages/security/src/core/index.ts
@@ -20,3 +20,35 @@ export interface CsrfToken {
   headerName: string;
   parameterName: string;
 }
+
+export enum Provider {
+  // oauth2
+  APPLE = 'APPLE',
+  DISCORD = 'DISCORD',
+  FACEBOOK = 'FACEBOOK',
+  GITHUB = 'GITHUB',
+  GOOGLE = 'GOOGLE',
+  LINKEDIN = 'LINKEDIN',
+  MICROSOFT = 'MICROSOFT',
+  SLACK = 'SLACK',
+  WECHAT = 'WECHAT',
+  X = 'X',
+  // contact
+  EMAIL = 'EMAIL',
+  PHONE = 'PHONE',
+}
+
+export const ALL_PROVIDERS = [
+  Provider.APPLE,
+  Provider.DISCORD,
+  Provider.FACEBOOK,
+  Provider.GITHUB,
+  Provider.GOOGLE,
+  Provider.LINKEDIN,
+  Provider.MICROSOFT,
+  Provider.SLACK,
+  Provider.WECHAT,
+  Provider.X,
+  Provider.EMAIL,
+  Provider.PHONE,
+] as const;
--- packages/security/src/index.ts
+++ packages/security/src/index.ts
@@ -3,8 +3,9 @@ export { OAuth2Client, oauth2RedirectQuerySchema } from './oauth2/client';
 export { RedisIndexedSessionRepository, RedisKVRepository } from './session/redis';
 export { DBIndexedSessionRepository } from './session/db';
 export { PRINCIPAL_NAME_INDEX_NAME } from './session/common';
+export { Provider, ALL_PROVIDERS } from './core/index';
 
-export type { Principal } from './core/index';
+export type { Principal, CsrfToken } from './core/index';
 export type { DBAdapter } from './session/db';
 export type { Namespace, SessionRepository, Session, KVRepository } from './session/types';
 export type { StandardClaims, OAuth2Token, UserInfo, OidcToken, OidcScopes } from './oauth2/types';
